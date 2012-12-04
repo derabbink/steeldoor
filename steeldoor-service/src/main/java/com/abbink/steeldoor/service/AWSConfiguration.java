453ae5f87d5161826f066b8106699dd454b1d244
Maarten Abbink <maarten@abbink.me>
2012-12-04 16:57:20 +0100

    wrote config structure for mysql

--- steeldoor-service/src/main/java/com/abbink/steeldoor/service/AWSConfiguration.java
+++ steeldoor-service/src/main/java/com/abbink/steeldoor/service/AWSConfiguration.java
@@ -1,14 +1,58 @@
 package com.abbink.steeldoor.service;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class AWSConfiguration {
 	@NotEmpty
 	@JsonProperty
-	private String foo;
+	private String dbEndpoint;
 	
-	public String getFoo() {
-		return foo;
+	@NotEmpty
+	@JsonProperty
+	private String dbInstanceId;
+	
+	@NotEmpty
+	@JsonProperty
+	private String dbMasterUser;
+	
+	@NotEmpty
+	@JsonProperty
+	private String dbMasterPassword;
+	
+	@NotEmpty
+	@JsonProperty
+	private String dbName;
+	
+	@Min(1)
+	@Max(65535)
+	@JsonProperty
+	private int dbPort;
+	
+	public String getDbEndpoint() {
+		return dbEndpoint;
+	}
+	
+	public String getDbInstanceId() {
+		return dbInstanceId;
+	}
+	
+	public String getDbMasterUser() {
+		return dbMasterUser;
+	}
+	
+	public String getDbMasterPassword() {
+		return dbMasterPassword;
+	}
+	
+	public String getDbName() {
+		return dbName;
+	}
+	
+	public int getDbPort() {
+		return dbPort;
 	}
 }
