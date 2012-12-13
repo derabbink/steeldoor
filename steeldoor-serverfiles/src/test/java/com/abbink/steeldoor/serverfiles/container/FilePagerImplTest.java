398256a93dca339ee4b106979e63ee1e06c3e754
Maarten Abbink <maarten@abbink.me>
2012-12-13 18:16:52 +0100

    changed file pager test to write multiple file tails

--- steeldoor-serverfiles/src/test/java/com/abbink/steeldoor/serverfiles/container/FilePagerImplTest.java
+++ steeldoor-serverfiles/src/test/java/com/abbink/steeldoor/serverfiles/container/FilePagerImplTest.java
@@ -35,13 +35,21 @@ public class FilePagerImplTest {
 	private File file;
 	private byte[] containerHeader1;
 	private byte[] containerHeader2;
+	private byte[] containerHeader3;
+	private byte[] containerHeader4;
 	private byte[] fileHeader;
-	private byte[] fileTailHeader;
+	private byte[] fileTailHeader1;
+	private byte[] fileTailHeader2;
+	private byte[] fileTailHeader3;
 	private byte[] dataComplete;
 	private byte[] data1;
 	private byte[] data2;
+	private byte[] data3;
+	private byte[] data4;
 	private byte[] checksum1;
 	private byte[] checksum2;
+	private byte[] checksum3;
+	private byte[] checksum4;
 	private int fileOwnerId = (4<<24)+(3<<16)+(2<<8)+1;
 	private long fileCookie = (101L<<56)+(102L<<48)+(103L<<40)+(104L<<32)+(105L<<24)+(106L<<16)+(107L<<8)+108L;
 	private long maxSize;
@@ -66,10 +74,12 @@ public class FilePagerImplTest {
 	}
 	
 	private void createData() {
-		dataComplete = new byte[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};
-		int half = (int) (dataComplete.length/2) + (overheadDiff/2);
-		data1 = Arrays.copyOfRange(dataComplete, 0, half);
-		data2 = Arrays.copyOfRange(dataComplete, half, dataComplete.length);
+		dataComplete = new byte[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39};
+		int step = (int) (dataComplete.length/4) - (overheadDiff/4);
+		data1 = Arrays.copyOfRange(dataComplete, 0, step+overheadDiff);
+		data2 = Arrays.copyOfRange(dataComplete, step+overheadDiff, 2*step+overheadDiff);
+		data3 = Arrays.copyOfRange(dataComplete, 2*step+overheadDiff, 3*step+overheadDiff);
+		data4 = Arrays.copyOfRange(dataComplete, 3*step+overheadDiff, dataComplete.length);
 	}
 	
 	private void createChecksums() throws NoSuchAlgorithmException {
@@ -78,6 +88,12 @@ public class FilePagerImplTest {
 		
 		md = MessageDigest.getInstance("SHA1");
 		checksum2 = md.digest(data2);
+		
+		md = MessageDigest.getInstance("SHA1");
+		checksum3 = md.digest(data3);
+		
+		md = MessageDigest.getInstance("SHA1");
+		checksum4 = md.digest(data4);
 	}
 	
 	private void createContainerProvider() {
@@ -88,8 +104,12 @@ public class FilePagerImplTest {
 	private void createHeaders() throws IOException {
 		containerHeader1 = TestableContainerWriter.generateHeader(maxSize, Container.SEALED);
 		containerHeader2 = TestableContainerWriter.generateHeader(maxSize, Container.SEALED);
+		containerHeader3 = TestableContainerWriter.generateHeader(maxSize, Container.SEALED);
+		containerHeader4 = TestableContainerWriter.generateHeader(maxSize, Container.SEALED);
 		fileHeader = TestableFileWriter.generateHeader(file.getTypeId(), file.getId(), file.getOwnerId(), file.getCookie(), FileInContainer.FILE_EXISTS, data1.length, file.getId()+1);
-		fileTailHeader = TestableFileTailWriter.generateHeader(FileTail.TYPE_ID, file.getId()+1, file.getId(), file.getOwnerId(), file.getCookie(), FileInContainer.FILE_EXISTS, data2.length, FileInContainer.NO_TAIL_ID);
+		fileTailHeader1 = TestableFileTailWriter.generateHeader(FileTail.TYPE_ID, file.getId()+1, file.getId(), file.getOwnerId(), file.getCookie(), FileInContainer.FILE_EXISTS, data2.length, file.getId()+2);
+		fileTailHeader2 = TestableFileTailWriter.generateHeader(FileTail.TYPE_ID, file.getId()+2, file.getId(), file.getOwnerId(), file.getCookie(), FileInContainer.FILE_EXISTS, data2.length, file.getId()+3);
+		fileTailHeader3 = TestableFileTailWriter.generateHeader(FileTail.TYPE_ID, file.getId()+3, file.getId(), file.getOwnerId(), file.getCookie(), FileInContainer.FILE_EXISTS, data2.length, FileInContainer.NO_TAIL_ID);
 	}
 	
 	@Test
@@ -100,6 +120,8 @@ public class FilePagerImplTest {
 		List<Container> containers = containerProvider.getRememberedContainers();
 		Container con1 = containers.get(0);
 		Container con2 = containers.get(1);
+		Container con3 = containers.get(2);
+		Container con4 = containers.get(3);
 		
 		//apache commons' implementation required
 		//container 1
@@ -113,16 +135,36 @@ public class FilePagerImplTest {
 		//container 2
 		bstream = new ByteArrayOutputStream();
 		bstream.write(containerHeader2);
-		bstream.write(fileTailHeader);
+		bstream.write(fileTailHeader1);
 		bstream.write(data2);
 		bstream.write(checksum2);
 		byte[] expected2 = bstream.toByteArray();
 		bstream.close();
+		//container 3
+		bstream = new ByteArrayOutputStream();
+		bstream.write(containerHeader3);
+		bstream.write(fileTailHeader2);
+		bstream.write(data3);
+		bstream.write(checksum3);
+		byte[] expected3 = bstream.toByteArray();
+		bstream.close();
+		//container 4
+		bstream = new ByteArrayOutputStream();
+		bstream.write(containerHeader4);
+		bstream.write(fileTailHeader3);
+		bstream.write(data4);
+		bstream.write(checksum4);
+		byte[] expected4 = bstream.toByteArray();
+		bstream.close();
 		
 		byte[] actual1 = FileUtils.readFileToByteArray(new java.io.File(con1.getFileName()));
 		byte[] actual2 = FileUtils.readFileToByteArray(new java.io.File(con2.getFileName()));
+		byte[] actual3 = FileUtils.readFileToByteArray(new java.io.File(con3.getFileName()));
+		byte[] actual4 = FileUtils.readFileToByteArray(new java.io.File(con4.getFileName()));
 		assertArrayEquals(expected1, actual1);
 		assertArrayEquals(expected2, actual2);
+		assertArrayEquals(expected3, actual3);
+		assertArrayEquals(expected4, actual4);
 	}
 	
 	@After
